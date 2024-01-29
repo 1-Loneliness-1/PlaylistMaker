95353d133aaa358563a2bf49dfe56eea67e69b76
Loneliness <costya.kisurin@yandex.ru>
2024-01-29 13:47:13 +0300

    Realized output of messages by pressing buttons.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        //Listener implementation for search button
+        val searchButtonListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажатие на кнопку поиска!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        searchButton.setOnClickListener(searchButtonListener)
+
+        //Listener implementation for media button
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажатие на кнопку медитека!", Toast.LENGTH_SHORT).show()
+        }
+
+        //Listener implementation for settings button
+        val settingsButtonListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажатие на кнопку настроек!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        settingsButton.setOnClickListener(settingsButtonListener)
     }
 }
