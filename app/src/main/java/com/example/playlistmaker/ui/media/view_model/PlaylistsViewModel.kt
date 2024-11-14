f9bd1620fae63382c45f67c32c7cc381ae54f0ff
1-Loneliness-1 <costya.kisurin@yandex.ru>
2024-11-14 11:43:52 +0300

    feat(ui_media): added logic of displaying interface elements and interactor interaction for the screen with playlists

--- app/src/main/java/com/example/playlistmaker/ui/media/view_model/PlaylistsViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/ui/media/view_model/PlaylistsViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.playlistmaker.ui.media.view_model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.playlistmaker.domain.media.PlaylistsInteractor
+import com.example.playlistmaker.domain.media.model.PlaylistScreenState
+import kotlinx.coroutines.launch
 
-class PlaylistsViewModel : ViewModel() {
+class PlaylistsViewModel(
+    private val playlistsInteractor: PlaylistsInteractor
+) : ViewModel() {
+
+    private val playlistScreenStateLiveData =
+        MutableLiveData<PlaylistScreenState>(PlaylistScreenState.EmptyState)
+
+    fun getPlaylistScreenStateLiveData(): LiveData<PlaylistScreenState> {
+        return playlistScreenStateLiveData
+    }
+
+    fun getAllPlaylistsFromDatabase() {
+        viewModelScope.launch {
+            playlistsInteractor
+                .getAllPlaylists()
+                .collect { playlists ->
+                    playlistScreenStateLiveData.postValue(
+                        if (playlists.isEmpty()) PlaylistScreenState.EmptyState
+                        else PlaylistScreenState.ContentState(playlists)
+                    )
+                }
+        }
+    }
+
 }
