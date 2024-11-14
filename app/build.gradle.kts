a7ca9fe0bf95a614f6ccb229a6bd9b916c5a04e4
1-Loneliness-1 <costya.kisurin@yandex.ru>
2024-11-14 11:14:43 +0300

    build(gradle): new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.activity:activity:1.9.3")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
